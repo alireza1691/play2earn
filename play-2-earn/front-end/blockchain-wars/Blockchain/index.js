40889e5a713f57699ba60d48824aaf56b7989748
Alireza <alirezashadow52555@gmail.com>
2023-11-09 14:33:46 +0330

    remove ts, add ustils

---
+++ play-2-earn/front-end/blockchain-wars/Blockchain/index.js
@@ -0,0 +1,16 @@
+import { townV2, landsV2, BMTAddress, faucet } from "./Addresses";
+import { abi as TownABI } from "./TownV2.json";
+import { abi as LandsABI } from "./LandsV2.json";
+import { abi as FaucetABI } from "./Faucet.json";
+import { abi as BMTABI } from "./BMT.json";
+
+export {
+  townV2,
+  landsV2,
+  BMTAddress,
+  faucet,
+  TownABI,
+  LandsABI,
+  FaucetABI,
+  BMTABI,
+};
