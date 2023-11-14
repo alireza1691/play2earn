770042cbd87629b7957722be604695dd2f25fff6
Alireza <alirezashadow52555@gmail.com>
2023-11-14 23:35:56 +0330

    nav

--- play-2-earn/front/blockdom/components/intro.tsx
+++ play-2-earn/front/blockdom/components/intro.tsx
@@ -4,11 +4,13 @@ import React from 'react'
 export default function Intro() {
   return (
     <section>
-      <div className='w-[50%] bg-white h-[10rem] '>
-        <div className=' w-[60%] right-0 flex justify-end ml-auto '>
-        <Image src={"/test.jpeg"} width={200} height={200} alt="sample" ></Image>
+      <div className='h-[10rem] md:w-[50%] sm:w-full '>
+        <div 
+        // className=' md:right-0 flex md:justify-end md:ml-auto  md:w-[60%] sm:w-full sm:justify-center sm:items-center'
+        className='block justify-center md:right-0 md:ml-auto md:w-[60%] md:justify-end'
+        >
+        <Image className=' justify-end flex right-0 ml-auto' src={"/test.jpeg"} width={200} height={200} alt="sample" ></Image>
         </div>
-
       </div>
       <div className='w-[50%] bg-slate-500'>
 
--- play-2-earn/front/blockdom/components/navbar.tsx
+++ play-2-earn/front/blockdom/components/navbar.tsx
@@ -2,35 +2,66 @@
 import dynamic from 'next/dynamic';
 import { motion } from 'framer-motion';
 import { ConnectWallet } from "@thirdweb-dev/react";
-import React from "react";
+import React, { Fragment, useState } from "react";
 import { links } from '@/lib/data';
 import Link from 'next/link';
+import { Dialog, Disclosure, Popover, Transition } from '@headlessui/react'
+import { ChevronDownIcon, PhoneIcon, PlayCircleIcon } from '@heroicons/react/20/solid'
+import {
+  ArrowPathIcon,
+  Bars3Icon,
+  ChartPieIcon,
+  CursorArrowRaysIcon,
+  FingerPrintIcon,
+  SquaresPlusIcon,
+  XMarkIcon,
+} from '@heroicons/react/24/outline'
+import Image from 'next/image';
+
 
 // const ConnectWallet = dynamic(() => import('@thirdweb-dev/react').then(module => module.ConnectWallet), { ssr: false });
 
+const products = [
+  { name: 'Analytics', description: 'Get a better understanding of your traffic', href: '#', icon: ChartPieIcon },
+  { name: 'Engagement', description: 'Speak directly to your customers', href: '#', icon: CursorArrowRaysIcon },
+  { name: 'Security', description: 'Your customers’ data will be safe and secure', href: '#', icon: FingerPrintIcon },
+  { name: 'Integrations', description: 'Connect with third-party tools', href: '#', icon: SquaresPlusIcon },
+  { name: 'Automations', description: 'Build strategic funnels that will convert', href: '#', icon: ArrowPathIcon },
+]
+const callsToAction = [
+{ name: 'Watch demo', href: '#', icon: PlayCircleIcon },
+{ name: 'Contact sales', href: '#', icon: PhoneIcon },
+]
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(' ')
+}
 
 
 export default function Navbar() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+
   return (
     <>
-    <header className="z-[999] relative">
+    {/* <header className="z-[999] relative">
       <motion.div className="fixed top-0 left-1/2 -translate-x-1/2 h-[4.5rem] w-full rounded-none border bg-[#365045] border-emerald-950 border-opacity-40 bg-opacity-75 shadow-lg shadow-white/[0.03] backdrop-blur-[0.5rem] sm:top-[1.5rem] sm:h-[3.5rem] sm:w-[36rem] sm:rounded-full "
       initial={{ y: -100, x: "-50%", opacity: 0 }}
       animate={{ y: 0, x: "-50%", opacity: 1 }}>
-        {/* <ConnectWallet/> */}
-      </motion.div>
-      <nav className='flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.75rem] sm:h-[initial] sm:py-0'>
+     
+      </motion.div> */}
+
+      {/* <nav className='flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.75rem] sm:h-[initial] sm:py-0 '>
         <ul className='flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-300 sm:w-[initial] sm:flex-nowrap sm:gap-5'>
           {links.map(link => (
             <motion.li key={link.hash}
-            className="h-3/4 flex items-center justify-center"
+            className="h-3/4 flex items-center justify-center relative"
             initial={{ y: -100, x: "-50%", opacity: 0 }}
             animate={{ y: 0, x: "-50%", opacity: 1 }}
              ><Link className='flex w-full items-center justify-center px-3 py-3 hover:text-gray-100 transition' href={link.hash}>{link.name}</Link></motion.li>
           ))}
         </ul>
-      </nav>
-      <nav className='flex fixed right-[1.5rem] top-[1.5rem]' >
+      </nav> */}
+      {/* <nav className='flex fixed right-[1.5rem] top-[1.5rem]' >
         <ConnectWallet      auth={{ loginOptional: false }}
             theme="dark"
             dropdownPosition={{
@@ -50,61 +81,176 @@ export default function Navbar() {
             }}
             switchToActiveChain={true}
             modalTitle="Login"
-            // style={{
-            //   backgroundColor: "#365045",
-            //   border: "none",
-            //   color: "white",
-            // }}
-            // detailsBtn={() => {
-            //   return <button className=" px-2 py-2 bg-[#365045]" >Connect</button>
-            // }}
+
             ></ConnectWallet></nav>
 
-    </header>
-    {/* <div className='w-full justify-end flex fixed'>  
-        <ConnectWallet      auth={{ loginOptional: false }}
-            theme="dark"
-            dropdownPosition={{
-              side: "bottom",
-              align: "center",
-            }}
-            btnTitle="Connect wallet"
-            modalSize="wide"
-            welcomeScreen={{
-              title:"Blockdom",
-              subtitle:"Welcome to Blockdom. Plase select & connect your wallet.",
-              img: {
-                src: "/blockdomLogo.png",
-                width: 100,
-                height: 100               
-              }
-            }}
-            switchToActiveChain={true}
-            modalTitle="Login"
-            // style={{
-            //   backgroundColor: "#365045",
-            //   border: "none",
-            //   color: "white",
-            // }}
-            // detailsBtn={() => {
-            //   return <button className=" px-2 py-2 bg-[#365045]" >Connect</button>
-            // }}
-            ></ConnectWallet>
-</div> */}
-    {/* <motion.div className='left-[6rem] top-[15rem] relative w-max  bg-[#365045] sm:rounded-full'>
+    </header> */}
+    <header className=" z-999 relative">
+      <div className='flex w-full fixed h-[5rem] bg-[#365045] top-0'></div>
+      <nav className="fixed mx-auto flex max-w-7xl items-center justify-between p-4 lg:px-8" aria-label="Global">
+        <div className="flex lg:flex-1">
+          <a href="#" className="-m-1.5 ">
+            <span className="sr-only">Your Company</span>
+            <Image className="h-10 w-auto" src="/BlockdomLogo.png" width={90} height={90} alt="" />
+          </a>
+        </div>
+        <div className="flex lg:hidden">
+          <button
+            type="button"
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+            onClick={() => setMobileMenuOpen(true)}
+          >
+            <span className="sr-only">Open main menu</span>
+            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+          </button>
+        </div>
+        <Popover.Group className="hidden lg:flex lg:gap-x-12">
+          <Popover className="relative">
+            <Popover.Button className="flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-50">
+              Product
+              <ChevronDownIcon className="h-5 w-5 flex-none text-gray-400" aria-hidden="true" />
+            </Popover.Button>
 
-    <ul className=' items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-300 sm:w-[initial] sm:flex-nowrap sm:gap-5'>
-          {links.map(link => (
-            <motion.li key={link.hash}
-            className="h-3/4 items-center justify-center mt-5 right-0"
-            initial={{ y: -100, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-             ><Link 
-             className='items-center justify-center px-3 py-3 hover:text-gray-500 transition'
-              href={link.hash}>{link.name}</Link></motion.li>
-          ))}
-        </ul>
-    </motion.div> */}
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-200"
+              enterFrom="opacity-0 translate-y-1"
+              enterTo="opacity-100 translate-y-0"
+              leave="transition ease-in duration-150"
+              leaveFrom="opacity-100 translate-y-0"
+              leaveTo="opacity-0 translate-y-1"
+            >
+              <Popover.Panel className="absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5">
+                <div className="p-4">
+                  {products.map((item) => (
+                    <div
+                      key={item.name}
+                      className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50"
+                    >
+                      <div className="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
+                        <item.icon className="h-6 w-6 text-gray-600 group-hover:text-indigo-600" aria-hidden="true" />
+                      </div>
+                      <div className="flex-auto">
+                        <a href={item.href} className="block font-semibold text-gray-900">
+                          {item.name}
+                          <span className="absolute inset-0" />
+                        </a>
+                        <p className="mt-1 text-gray-600">{item.description}</p>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+                <div className="grid grid-cols-2 divide-x divide-gray-900/5 bg-gray-50">
+                  {callsToAction.map((item) => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      className="flex items-center justify-center gap-x-2.5 p-3 text-sm font-semibold leading-6 text-gray-900 hover:bg-gray-100"
+                    >
+                      <item.icon className="h-5 w-5 flex-none text-gray-400" aria-hidden="true" />
+                      {item.name}
+                    </a>
+                  ))}
+                </div>
+              </Popover.Panel>
+            </Transition>
+          </Popover>
+
+          <a href="#" className="text-sm font-semibold leading-6 text-gray-50">
+            Features
+          </a>
+          <a href="#" className="text-sm font-semibold leading-6 text-gray-50">
+            Marketplace
+          </a>
+          <a href="#" className="text-sm font-semibold leading-6 text-gray-50">
+            Company
+          </a>
+        </Popover.Group>
+        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+          <ConnectWallet/>
+        </div>
+      </nav>
+      <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+        <div className="fixed inset-0 z-10" />
+        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <div className="flex items-center justify-between">
+            <a href="#" className="-m-1.5 p-1.5">
+              <span className="sr-only">Your Company</span>
+              <img
+                className="h-8 w-auto"
+                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                alt=""
+              />
+            </a>
+            <button
+              type="button"
+              className="-m-2.5 rounded-md p-2.5 text-gray-700"
+              onClick={() => setMobileMenuOpen(false)}
+            >
+              <span className="sr-only">Close menu</span>
+              <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+            </button>
+          </div>
+          <div className="mt-6 flow-root">
+            <div className="-my-6 divide-y divide-gray-500/10">
+              <div className="space-y-2 py-6">
+                <Disclosure as="div" className="-mx-3">
+                  {({ open }) => (
+                    <>
+                      <Disclosure.Button className="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
+                        Product
+                        <ChevronDownIcon
+                          className={classNames(open ? 'rotate-180' : '', 'h-5 w-5 flex-none')}
+                          aria-hidden="true"
+                        />
+                      </Disclosure.Button>
+                      <Disclosure.Panel className="mt-2 space-y-2">
+                        {[...products, ...callsToAction].map((item) => (
+                          <Disclosure.Button
+                            key={item.name}
+                            as="a"
+                            href={item.href}
+                            className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                          >
+                            {item.name}
+                          </Disclosure.Button>
+                        ))}
+                      </Disclosure.Panel>
+                    </>
+                  )}
+                </Disclosure>
+                <a
+                  href="#"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >
+                  Features
+                </a>
+                <a
+                  href="#"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >
+                  Marketplace
+                </a>
+                <a
+                  href="#"
+                  className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >
+                  Company
+                </a>
+              </div>
+              <div className="py-6">
+                <a
+                  href="#"
+                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >
+                  Log in
+                </a>
+              </div>
+            </div>
+          </div>
+        </Dialog.Panel>
+      </Dialog>
+    </header>
     </>
   );
 }
