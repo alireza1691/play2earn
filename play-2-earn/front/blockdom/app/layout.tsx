b235b21307bd50831c0ba24819c090047dd5baa4
Alireza <alirezashadow52555@gmail.com>
2023-11-16 14:27:55 +0330

    intro responsive

--- play-2-earn/front/blockdom/app/layout.tsx
+++ play-2-earn/front/blockdom/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
 
     <html lang="en">
-      <body className={`${inter.className} bg-black text-yellow-50 relative h-[5000px]  pt-[7rem]`}>
+      <body className={`${inter.className}  bg-black text-yellow-50 relative h-[5000px]  pt-[7rem]`}>
         <div className="bg-[#7FDDAD] absolute top-[-13rem] -z-10 left-[0rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[13rem] sm:w-[15.75rem] sm:left-[3rem] sm:blur-[10rem]  opacity-50 lg:left-[10rem] lg:w-[22.5rem]" ></div>
         {/* <div className="bg-yellow-900 absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[30rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div> */}
         <ThirdwebProvider>
--- play-2-earn/front/blockdom/components/intro.tsx
+++ play-2-earn/front/blockdom/components/intro.tsx
@@ -1,36 +1,66 @@
+import { motion } from "framer-motion";
 import Image from "next/image";
 import React from "react";
+import grid from "../public/grid.svg"
 
 export default function Intro() {
   return (
-    <section>
-      <div className="w-full md:w-[50%] h-[30rem]  ">
+    <section className="flex flex-wrap sm:p-0 h-auto">
+      <div className="w-full md:w-[45%]  flex flex-col items-center  ">
         <div
           // className=' md:right-0 flex md:justify-end md:ml-auto  md:w-[60%] sm:w-full sm:justify-center sm:items-center'
-          className="w-full  h-[20rem] justify-center items-center md:w-[60%] md:object-right  md:ml-auto "
+          className="w-full px-5 sm:px-0 justify-center items-center 2xl:w-[60%] md:object-right  md:ml-auto mt-[3rem] md:pl-[6rem]  md:pr-[1rem] z-10"
         >
-          <div className="">
+          <motion.div className="flex flex-col items-center md:items-left "
+          initial={{ opacity: 0, scale: 0 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ type: "tween", duration: 0.2}}
+          >
+        
             {" "}
             <Image
-              className=" flex justify-center right-0 object-center w-[90%] rounded-[1rem] md:mr-[1rem]  "
+              className=" right-0 object-center  rounded-[1rem] w-  "
               src={"/test.jpeg"}
-              width={200}
-              height={200}
+              width={477}
+              height={416}
                 // fill
               alt="sample"
             ></Image>
-          </div>
-          <div>
+          </motion.div>
+          <motion.div initial={{ opacity: 0, scale: 0 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{
+            type: "spring",
+            stiffness: 125,
+            delay: 0.1,
+            duration: 0.5,
+          }}>
             <h1 className=" text-[64px]">Lorem ipsum dolor sit amet</h1>
             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur hic saepe eaque asperiores, maxime quaerat ad harum eveniet? Praesentium dolores autem soluta illum, iure assumenda saepe consectetur. Eveniet, repellendus ut.</p>
-          </div>
+          </motion.div>
           <div className="mt-8">
-            <button className="py-3 px-7 mr-4 tex bg-gradient-to-l  to-[#75E5F4] from-[#9FFFCF] border-[1px] border-[#75E5F4] text-black rounded-[0.8rem] text-[0.8rem] font-semibold  hover:from-[#9168FB] hover:to-[#9168FB] hover:border-[#9168FB]">Get start</button>
-            <button className="py-3 px-7 mr-4 tex border-neutral-50 border-[1px] text-[0.8rem] rounded-[0.8rem] font-semibold ">Check the Documentation</button>
+            <button className="py-3 px-7 mr-4 tex bg-gradient-to-l  to-[#75E5F4] from-[#9FFFCF]  text-black rounded-[0.8rem] text-[0.8rem] font-semibold   hover:from-[#9168FB] hover:to-[#9168FB] hover:border-[#9168FB] ">Get start</button>
+            <a className="py-[14px] px-7 mr-4 tex border-neutral-50 border-[1px] text-[0.8rem] rounded-[0.8rem] font-semibold " href="https://blockdom.app" target={"_blank"} >Check the Documentation</a>
           </div>
         </div>
       </div>
-      <div className="w-[50%] bg-slate-500"></div>
+      <div className="w-full max-w-[50rem] md:w-[55%] pt-[4rem] sm:pt-0 md:pt-0 lg:pt-0 xl:pt-0 2xl:pt-0 flex flex-col items-center gap-5 z-10 pl-[3rem] pr-[6rem] min-h-[40rem]">
+          <div className="flex h-[50%] w-full bg-white/20 gap-5">
+            <div className=" w-[50%] h-full flex flex-col gap-5 ">
+                <div className=" bg-white/50 h-[50%] w-full"></div>
+                <div className=" bg-white/30 h-[50%] w-full"></div>
+            </div>
+            <div className="w-[50%] bg-white/40">
+                
+            </div>
+          </div>
+          <div className="h-[50%] w-full bg-white/10">
+
+          </div>
+      </div>
+      <div className=" flex flex-col items-center invisible sm:visible w-full h-[30rem]  absolute mt-[10rem]">        <Image className=" rounded-full opacity-70 brightness-140 w-[574px] h-[660px]" src={grid} width={300} height={300} objectFit="cover" alt="Grid" />
+</div>
     </section>
-  );
+
+  );    
 }
