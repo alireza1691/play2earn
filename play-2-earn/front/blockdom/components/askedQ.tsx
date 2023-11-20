d139a50006a51e5408da2c19914643068f0d7639
Alireza <alirezashadow52555@gmail.com>
2023-11-20 18:36:39 +0330

    footer complete

--- play-2-earn/front/blockdom/components/askedQ.tsx
+++ play-2-earn/front/blockdom/components/askedQ.tsx
@@ -1,35 +1,63 @@
-"use client"
-import React, { useState } from 'react'
-import { questions } from "@/lib/data"
+"use client";
+import React, { useState } from "react";
+import { questions } from "@/lib/data";
 
 export default function Questions() {
-    const [openedQ, setOpenedQ] = useState<number | null>(null)
+  const [openedQ, setOpenedQ] = useState<number | null>(null);
   return (
-    <section className='w-full px-10 sm:px-[20%] xl:px-[25%] flex flex-col'>
-      <h2 className=' text-[36px] text-white text-center font-bold'>Frequently Asked Questions</h2>
-      
-      <div className='min-h-20  mt-20 flex flex-col gap-4 relative overflow-hidden'>
-      {/* <div className=' W-[10rem] h-[10rem] absolute'> */}
-        {/* <div className='w-[20rem] h-[20rem] left-[-10rem] bg-light-green-800 absolute rounded-full blur-[5rem] -z-10'></div> */}
-        {/* </div> */}
-      {questions.map((question, index) => (
-        <React.Fragment key={index}>
-        {/* <div className='w-[20rem] h-[20rem] left-[-10rem] bg-light-green-800 absolute rounded-full blur-[5rem] -z-10'></div> */}
-        <div  className={" flex flex-col overflow-hidden bg-gray-50/10 py-3 px-4 rounded-[0.5rem] relative "}>
-           
-            <div className='w-[20rem] h-[20rem] top-[-5rem] left-[-10rem] bg-green-800 absolute rounded-full blur-[8rem] -z-10'></div>
-            <div className=' flex justify-between'>
-            <h2 className=' text-[14px] flex items-center'>{question.title}</h2>
-            {openedQ == index ? <a className=' cursor-pointer p-1' onClick={() => setOpenedQ(null)}>&#8963;</a> : <a className=' cursor-pointer p-1' onClick={() => setOpenedQ(index)}>&gt;</a> }
+    <section className=" transition-all duration-1000 ease-out w-full px-10 sm:px-[20%] xl:px-[25%] flex flex-col">
+      <h2 className=" text-[36px] text-white text-center font-bold">
+        Frequently Asked Questions
+      </h2>
+
+      <div className="min-h-20  mt-20 flex flex-col gap-4 relative overflow-hidden">
+        {questions.map((question, index) => (
+          <React.Fragment key={index}>
+            <div
+              onClick={() => setOpenedQ(openedQ == index ? null : index)}
+                className={
+                `${openedQ === index ? 'max-h-[20rem] transition-all duration-1000 ease-out' : 'transition-all duration-1000 ease-in max-h-[3.5rem] overflow-hidden'}  !cursor-pointer  overflow-hidden bg-gray-50/10 py-3 px-4 rounded-[0.5rem] relative `
+              }
+            //   className={
+            //     `${openedQ === index ? 'max-h-[20rem] transition-max-h duration-1000 ease-out' : 'transition-min-h duration-1000 ease-in max-h-[3.5rem] overflow-hidden'}  !cursor-pointer flex flex-col overflow-hidden bg-gray-50/10 py-3 px-4 rounded-[0.5rem] relative `
+            //   }
+            // className={`!cursor-pointer relative bg-green-800 rounded-[0.5rem] py-3 px-4 transition-max-h duration-1000 ease-out ${
+            //     openedQ === index ? 'max-h-[20rem]' : 'max-h-[3.5rem] overflow-hidden'
+            //   }`}
+            // className={
+            //     `${openedQ === index ? 'max-h-[20rem] transition-max-h duration-1000 ease-out' : 'transition-max-h duration-1000 ease-out max-h-[3.5rem] overflow-hidden'}  !cursor-pointer flex flex-col overflow-hidden bg-gray-50/10 py-3 px-4 rounded-[0.5rem] relative `
+            //   }
+            >
+              <div className="w-[20rem] h-[20rem] top-[-5rem] left-[-10rem] bg-green-800 absolute rounded-full blur-[8rem] -z-10"></div>
+              <div className="transition-all duration-1000 ease-out flex justify-between">
+                <h2 className="text-[14px] flex items-center ">
+                  {question.title}
+                </h2>
+                {openedQ == index ? (
+                  <a
+                    className=" cursor-pointer p-1"
+                    onClick={() => setOpenedQ(null)}
+                  >
+                    &#8963;
+                  </a>
+                ) : (
+                  <a
+                    className=" cursor-pointer p-1"
+                    onClick={() => setOpenedQ(index)}
+                  >
+                    &gt;
+                  </a>
+                )}
+              </div>
+              {openedQ == index && (
+                <p className="transition-all duration-1000 ease-out text-[12px] px-3 mt-3 font-light text-white opacity-80">
+                  {question.answer}
+                </p>
+              )}
             </div>
-            {openedQ == index &&        <p className=' text-[12px] px-3 mt-3 font-light text-white opacity-80'>{question.answer}</p>}
-     
-           
-        </div>
-        </React.Fragment>
-      ))}
+          </React.Fragment>
+        ))}
       </div>
-    
     </section>
-  )
+  );
 }
--- play-2-earn/front/blockdom/components/footer.tsx
+++ play-2-earn/front/blockdom/components/footer.tsx
@@ -1,26 +1,60 @@
-import React from 'react'
+import React from "react";
 import { AiFillTwitterCircle } from "react-icons/ai";
 import { RiTelegramFill } from "react-icons/ri";
 import { FaDiscord } from "react-icons/fa6";
+import { FaAngleRight } from "react-icons/fa6";
+import { FaAngleUp } from "react-icons/fa6";
+
+import Image from "next/image";
 
 export default function Footer() {
   return (
-    <section className=' flex flex-col bg-[#213830] bottom-0 mt-[15rem]'>
-        <div className='flex  justify-center gap-5 text-[26px] py-5'>
-            <AiFillTwitterCircle/>
-            <FaDiscord/>
-            <RiTelegramFill/>
+    <section className=" flex flex-col bg-[#213830] bottom-0 mt-[15rem]">
+      <div className="flex  justify-center gap-5 text-[26px] py-5">
+        <AiFillTwitterCircle />
+        <FaDiscord />
+        <RiTelegramFill />
+      </div>
+      <div className="flex flex-wrap sm:flex-row mt-[4rem]">
+        <div className="w-full sm:w-[55%] sm:px-[13.5rem] ">
+          <h2 className="text-[28px] mb-5 text-[#A5F4B6] font-semibold">
+            Join our community
+          </h2>
+          <p className="text-[14px] opacity-60 text-white">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero
+            neque quod est architecto reprehenderit unde iusto laborum
+            exercitationem ipsum. Tenetur qui laudantium esse ratione.
+            Accusantium, nulla. Esse facere distinctio cum.
+          </p>
+          <div className=" w-fit flex flex-row relative mt-8">
+            {" "}
+            <input
+              placeholder="Email"
+              className=" text-white bg-white/20  py-3 px-4 rounded-2xl border-[0.2rem] border-white/10   "
+            ></input>
+            <FaAngleRight className=" cursor-pointer text-[40px] p-2 text-black bg-white absolute right-2 top-[8px] rounded-xl" />
+          </div>
         </div>
-        <div className='flex flex-wrap'>
-        <div className='w-full sm:w-1/2 sm:px-[12.5rem] py-[4rem]'>
-        <h2 className='text-[28px] mb-5 text-[#A5F4B6] font-semibold' >Join our community</h2>
-        <p className='text-[14px] opacity-60 text-white'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero neque quod est architecto reprehenderit unde iusto laborum exercitationem ipsum. Tenetur qui laudantium esse ratione. Accusantium, nulla. Esse facere distinctio cum.</p>
-        <input placeholder='Email' className=' text-white bg-white/20 mt-8 py-3 px-4 rounded-2xl border-[0.2rem] border-white/10   '></input>
+        <div className="w-full sm:w-[45%] px-[4rem] flex flex-wrap gap-10">
+          <div className=" flex flex-col gap-4">
+            <h2 className=" text-[22px] font-bold">Support</h2>
+            <h3 className=" opacity-70">dsfsdsdf</h3>
+            <h3 className=" opacity-70">sdfsdf</h3>
+            <h3 className=" opacity-70">sdjfklsjanlk</h3>
+          </div>
+          <div className=" flex flex-col gap-4">
+            <h2 className=" text-[22px] font-bold">Terms</h2>
+            <h3 className=" opacity-70">dsfsdsdf</h3>
+            <h3 className=" opacity-70">sdfsdf</h3>
+            <h3 className=" opacity-70">sdjfklsjanlk</h3>
+          </div>
         </div>
-        <div className='w-full sm:w-1/2'>
-        
-        </div>
-        </div>
+      </div>
+      <div className=" flex flex-col sm:flex-row justify-around items-center p-10 mt-10 relative" >
+        <Image src={"/"} width={200} height={50} alt={"logo"}></Image>
+        <p className="absolute">@2024 Blockdom. All right reserved</p>
+        <a className="p-4 bg-[#62AE94] rounded-full"><FaAngleUp/></a>
+      </div>
     </section>
-  )
+  );
 }
