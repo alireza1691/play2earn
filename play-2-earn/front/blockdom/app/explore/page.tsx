27e3cf62fd577832b843e30d5c31f0d9e18dae27
Alireza <alirezashadow52555@gmail.com>
2023-12-10 10:32:36 +0330

    comment parcels

--- play-2-earn/front/blockdom/app/explore/page.tsx
+++ play-2-earn/front/blockdom/app/explore/page.tsx
@@ -3,6 +3,7 @@
 import Slidebar from "@/components/exploreComponents/slidebar";
 import Attack from "@/components/gameComponents.tsx/attack";
 import SelectedParcel from "@/components/gameComponents.tsx/selectedParcel";
+
 import WorldVector from "@/svg/worldVector";
 import Image from "next/image";
 import React, { useState } from "react";
@@ -12,7 +13,7 @@ type Item = {
   y: number;
 };
 
-export default function Explore() {
+export default function Explore2() {
   const [slidebar, setSlidebar] = useState(false);
   const [attackBox, setAttackBox] = useState(false);
   const [selectedLand, setSelectedLand] = useState<number | null>(null)
@@ -21,9 +22,9 @@ export default function Explore() {
 
   return (
     <>
-          <div className="z-0 right-0  absolute w-screen h-screen lg:w-[976px] 2xl:w-[1440px] overflow-hidden lg:bg-[url('/svg/gameItems/bg.svg')] bg-[url('/BGMobile.jpg')] bg-cover bg-center bg-no-repeat brightness-75 blur-sm "></div>
+        
 
-          <SelectedParcel setSelectedLand={setSelectedLand} setSlideBar={setSlidebar}/>
+          {/* <SelectedParcel setSelectedLand={setSelectedLand} setSlideBar={setSlidebar}/> */}
           
       {/* sm:w-[480px]  md:w-[768px] lg:w-[976px] */}
         {/* <Image
@@ -33,80 +34,20 @@ export default function Explore() {
           height={629}
           alt="deactivedParcel"
         /> */}
-        <div className=" absolute h-screen w-screen overflow-hidden">
-        <Image
-          className=" absolute left-20 -translate-x-1/2 md:top-0 -top-[7.5rem] h-[20rem]  w-auto  "
-          src={"/svg/gameItems/cloudTL.svg"}
-          width={698}
-          height={467}
-          alt="tlcloud"
-        />
-        <Image
-          className=" absolute -right-10 translate-x-1/2  md:top-0 -top-[7.5rem] h-[20rem] lg:-top-20  w-auto  z-10 "
-          src={"/svg/gameItems/cloudTR.svg"}
-          width={698}
-          height={467}
-          alt="trcloud"
-        />
-        <Image
-          className=" absolute -left-20  translate-y-1/2 bottom-0  h-[20rem]  w-auto  "
-          src={"/svg/gameItems/cloudBL.svg"}
-          width={698}
-          height={467}
-          alt="deactivedParcel"
-        />
-        <Image
-          className=" absolute translate-y-1/2 bottom-0  -right-20 h-[20rem]  w-auto  "
-          src={"/svg/gameItems/cloudBR.svg"}
-          width={1008}
-          height={629}
-          alt="deactivedParcel"
-        />
+        {/* <div className=" absolute h-screen w-screen overflow-hidden">
+       
         </div>
-     
-        {/* <a className=" cursor-pointer z-50 left-[21.5rem] bottom-[14rem] absolute">
-        <Image src={"/svg/gameItems/bottomLeftArrow.svg"} width={65} height={65} alt="arrow"/>
-        </a>
-        <a className="cursor-pointer z-50 right-[22.5rem] bottom-[15rem] absolute">
-        <Image src={"/svg/gameItems/bottomRightArrow.svg"} width={60} height={60} alt="arrow"/>
-        </a>
-        <a className="cursor-pointer z-50 right-[23.5rem] top-[22rem] absolute">
-        <Image src={"/svg/gameItems/topRightArrow.svg"} width={60} height={60} alt="arrow"/>
-        </a>
-        <a className=" cursor-pointerz-50 left-[22.5rem] top-[23rem] absolute">
-        <Image src={"/svg/gameItems/topLeftArrow.svg"} width={60} height={60} alt="arrow"/>
-        </a> */}
+   */}
         <a className=" transition-all cursor-pointer shadow-md hover:bg-[#06291D]/40 font- text-[#98FBD7] top-[10rem] z-10 flex flex-row items-center gap-3 rounded-md left-1/2 -translate-x-1/2 absolute bg-[#06291D]/50 px-4 py-3"><WorldVector/> Go to world map</a>
      
 
-      <main>
+      <main className=" flex flex-row justify-center">
+      {/* <div className="z-0  relative w-screen  md:w-[768px] h-[50rem] lg:w-[976px] 2xl:w-[1440px] overflow-y-scroll overflow-x-scroll brightness-75">
+            <Image className=" blur-sm !w-auto !h-[60rem] object-cover absolute " src={'/svg/gameItems/bg.svg'} width={400} height={400} alt="bg"/>
+          </div> */}
         <Slidebar slidebar={slidebar} setSlidebar={setSlidebar} selectedLand={selectedLand} />
         {attackBox && <Attack />}
      
-        {/* <div className=" p-4">
-          <ul className="flow-root space-y-4 relative top-[10rem]">
-            <li className="bg-white p-2 overflow-hidden relative">
-              Item 1
-              <div className=" absolute bg-green-700  blur-[5rem] w-[15rem] h-[15rem] rounded-full left-0 top-10 z-50"></div>
-            </li>
-            <li className="bg-white p-2 relative overflow-hidden">
-              Item 2
-              <div className=" absolute bg-green-700  blur-[5rem] w-[15rem] h-[15rem] rounded-full left-0 -top-8 z-50"></div>
-            </li>
-            <li className="bg-white p-2   overflow-hidden relative">
-              Item 3
-              <div className=" absolute bg-green-700  blur-[5rem] w-[15rem] h-[15rem] rounded-full left-0 -top-28 z-50"></div>
-            </li>
-            <li className="bg-white  p-2 overflow-hidden relative">
-              Item 4{" "}
-              <div className=" absolute bg-green-700  blur-[5rem] w-[15rem] h-[15rem] rounded-full left-0 -top-40 z-50"></div>
-            </li>
-            <li className="bg-white p-2 overflow-hidden relative">
-              Item 5{" "}
-              <div className=" absolute bg-green-700  blur-[5rem] w-[15rem] h-[15rem] rounded-full left-0 -top-56 z-50"></div>
-            </li>
-          </ul>
-        </div> */}
       </main>
     </>
   );
--- play-2-earn/front/blockdom/components/gameComponents.tsx/selectedParcel.tsx
+++ play-2-earn/front/blockdom/components/gameComponents.tsx/selectedParcel.tsx
@@ -1,7 +1,9 @@
 "use client";
 import Image from "next/image";
-import React, { useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import {Tooltip, Button} from "@nextui-org/react";
+import ParcelsWideMode from "./parcelsWideMode";
+import ParcelsMobileMode from "./parcelsMobileMode";
 
 type landSelectorHookProps = {
   setSelectedLand: React.Dispatch<React.SetStateAction<number | null>>;
@@ -12,29 +14,63 @@ type selectedParcelType = {
   y: number;
 };
 
-export default function SelectedParcel({
+export default function SelectedParcel2({
   setSelectedLand,
   setSlideBar,
 }: landSelectorHookProps) {
+
+const containerRef = useRef<HTMLDivElement >(null);
+
+
   const [selectedParcel, setSelectedParcel] = useState<selectedParcelType>({
     x: 100,
     y: 100,
   });
-  const lands = (xFrom: number, yFrom: number) => {
-    let items = [];
-    for (let x = xFrom; x < xFrom + 10; x++) {
-      for (let y = yFrom; y < yFrom + 10; y++) {
-        items.push(Number(x.toString() + y.toString()));
-      }
-    }
-    return items;
-  };
 
-  const separatedCoordinate = (coordinate:string) =>{
-    const middleIndex = Math.floor(coordinate.length / 2);
-    const result = coordinate.slice(0, middleIndex) + " - " + coordinate.slice(middleIndex);
-    return result
-  }
+//   const inViewParcels = (selectedParcel:selectedParcelType): selectedParcelType[] => {
+//     let parcelsArray = []
+
+//     parcelsArray[0] = {...selectedParcel, x : selectedParcel.x -10, y: selectedParcel.y - 10}
+//     parcelsArray[1] = {...selectedParcel,  y: selectedParcel.y - 10}
+//     parcelsArray[2] = {...selectedParcel, x : selectedParcel.x + 10,  y: selectedParcel.y - 10}
+//     parcelsArray[3] = {...selectedParcel, x : selectedParcel.x - 10}
+//     parcelsArray[4] = selectedParcel
+//     parcelsArray[5] = {...selectedParcel, x : selectedParcel.x + 10}
+//     parcelsArray[6] = {...selectedParcel, x : selectedParcel.x - 10, y: selectedParcel.y + 10}
+//     parcelsArray[7] = {...selectedParcel, y: selectedParcel.y + 10}
+//     parcelsArray[8] = {...selectedParcel, x : selectedParcel.x + 10, y: selectedParcel.y + 10}
+
+//     return parcelsArray
+//   }
+
+
+//   const parcelLands = (xFrom: number, yFrom: number) => {
+//     let items = [];
+//     for (let x = xFrom; x < xFrom + 10; x++) {
+//       for (let y = yFrom; y < yFrom + 10; y++) {
+//         items.push(Number(x.toString() + y.toString()));
+//       }
+//     }
+//     return items;
+//   };
+
+//   const separatedCoordinate = (coordinate:string) =>{
+//     const middleIndex = Math.floor(coordinate.length / 2);
+//     const result = coordinate.slice(0, middleIndex) + " - " + coordinate.slice(middleIndex);
+//     return result
+//   }
+
+  useEffect(() => {
+    const container = containerRef.current!;
+
+    // Calculate the scroll position to center the content
+    const scrollLeft = (container.scrollWidth - container.clientWidth) / 2;
+    const scrollTop = (container.scrollHeight - container.clientHeight) / 2;
+
+    // Scroll the container to the center position
+    container.scrollTo(scrollLeft, scrollTop);
+  }, []);
+  
   return (
     <>
       <div className="z-20 absolute top-[4.5rem] h-[3rem] w-full greenHeaderGradient items-center flex justify-center ">
@@ -44,11 +80,73 @@ export default function SelectedParcel({
           {selectedParcel.y}
         </h3>
       </div>
-      <section className="z-10 absolute top-[12.5rem] lg:top-[10.25rem] left-1/2 lg:left-auto 2xl:right-2 lg:-right-[11rem] -translate-x-1/2  2xl:top-[9rem] ">
-      {/* {selectedParcel.x <= 100 && (
-          <div className=" -z-10 transition-all absolute  bg-black h-[100rem] w-[50rem] 2xl:-top-[47rem] -left-[45rem] -top-[49rem] lg:-left-[35rem] lg:-top-[48rem] viewGrid !shadow-none blur-md"></div>
-        )} */}
-        <a
+      <div ref={containerRef} className="w-[100vw] h-[100vh] overflow-scroll flex items-center justify-center object-cover relative">
+ 
+      {/* <div className=" invisible md:visible absolute grid gap-[1px] w-[1080px] md:w-[1590px] 2xl:w-[2130px] transform grid-cols-3 md:left-[20rem] 2xl:left-[27.5rem] top-0 viewGrid ">
+            {parcels(selectedParcel).map((parcel,key) =>(
+                <div key={key} className={`grid w-fit grid-cols-10 gap-[1px] ${key == 4 ? "":"blur-md brightness-50"}`}>
+                    {lands(selectedParcel.x, selectedParcel.y).map((land,index) => (
+                               <Tooltip radius="sm" key={key} color={"default"} content={separatedCoordinate(land.toString())} className={`capitalize  !bg-[#06291D]/80 ${key != 4 && "invisible"}`}>
+                               <a
+                                 key={index}
+                                 onClick={() =>  
+                                   {key == 4 && setSelectedLand(land),key == 4 && setSlideBar(true),console.log(land);
+                                   }
+                                 }
+                                 className={`${key == 4 ? "cursor-pointer hover:bg-blue-gray-900/10" : "cursor-default"} text-black text-[8px] w-[35px] h-[35px] md:h-[52px] md:w-[52px] 2xl:h-[70px] 2xl:w-[70px]  shadow-md `} 
+                               >
+                                 <Image
+                                   className=" h-[35px] w-[35px] md:h-[52px] md:w-[52px] 2xl:h-[70px] 2xl:w-[70px] absolute -z-10"
+                                   src={"/parcels/parcel.png"}
+                                   width={60}
+                                   height={60}
+                                   alt="parcel"
+                                   quality={30}
+                                 />
+                                 {land}
+                               </a>
+                               </Tooltip>
+                    ))}
+              
+                </div>
+            ))}
+        </div> */}
+        <ParcelsWideMode setSlideBar={setSlideBar} setSelectedLand={setSelectedLand} setSelectedParcel={setSelectedParcel} selectedParcel={selectedParcel} />
+        <ParcelsMobileMode setSlideBar={setSlideBar} setSelectedLand={setSelectedLand} setSelectedParcel={setSelectedParcel} selectedParcel={selectedParcel} />
+        {/* <div  className=" absolute grid gap-[1px] w-[1590px]  transform grid-cols-3 left-[0rem] top-0 md:invisible ">
+            {inViewParcels(selectedParcel).map((parcel,key) =>(
+                <div key={key} className={`grid w-fit grid-cols-10 gap-[1px] ${key == 4 ? "":"blur-md brightness-50"}`}>
+                    {parcelLands(selectedParcel.x, selectedParcel.y).map((land,index) => (
+                               <Tooltip radius="sm" key={key} color={"default"} content={separatedCoordinate(land.toString())} className={`capitalize  !bg-[#06291D]/80 ${key != 4 && "invisible"}`}>
+                               <a
+                                 key={index}
+                                 onClick={() =>  
+                                   {key == 4 && setSelectedLand(land),key == 4 && setSlideBar(true),console.log(land);
+                                   }
+                                 }
+                                 className={`${key == 4 ? "cursor-pointer hover:bg-blue-gray-900/10" : "cursor-default"} text-black text-[8px] h-[52px] w-[52px] 2xl:h-[70px] 2xl:w-[70px]  shadow-md `} 
+                               >
+                                 <Image
+                                   className=" h-[52px] w-[52px] absolute -z-10"
+                                   src={"/parcels/parcel.png"}
+                                   width={60}
+                                   height={60}
+                                   alt="parcel"
+                                   quality={30}
+                                 />
+                                 {land}
+                               </a>
+                               </Tooltip>
+                    ))}
+              
+                </div>
+            ))}
+        </div> */}
+      </div>
+     
+      <section className="z-10 absolute   ">
+  
+        {/* <a
           onClick={() => {
             selectedParcel.y < 190 &&
               setSelectedParcel((prevState) => ({
@@ -123,8 +221,8 @@ export default function SelectedParcel({
             height={60}
             alt="arrow"
           />
-        </a>
-        <div className="z-10 grid gap-[1px] grid-cols-10 transform viewGrid w-max shadow-black shadow-large ">
+        </a> */}
+        {/* <div className="z-10 grid gap-[1px] grid-cols-10 transform viewGrid w-max shadow-black shadow-large ">
           {lands(selectedParcel.x, selectedParcel.y).map((land, key) => (
              <Tooltip radius="sm" key={key} color={"default"} content={separatedCoordinate(land.toString())} className="capitalize  !bg-[#06291D]/80">
             <a
@@ -146,7 +244,8 @@ export default function SelectedParcel({
             </a>
             </Tooltip>
           ))}
-        </div>
+        </div> */}
+
       </section>
     </>
   );
