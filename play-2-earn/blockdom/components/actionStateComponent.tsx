25fa2ff692a9944f54d48cd5d680854dda3168e6
Alireza <alirezashadow52555@gmail.com>
2023-12-27 17:51:53 +0330

    transaction state popups

--- play-2-earn/blockdom/components/actionStateComponent.tsx
+++ play-2-earn/blockdom/components/actionStateComponent.tsx
@@ -1,16 +1,21 @@
 "use client"
 import { useBlockchainStateContext } from '@/context/blockchain-state-context'
+import BlockdomLogo from '@/svg/blockdomLogo'
+import DoneIcon from '@/svg/doneIcon'
+import FailedIcon from '@/svg/failedIcon'
+import RejectIcon from '@/svg/rejectIcon'
+import { Spinner } from '@nextui-org/react'
 import React from 'react'
 
 export default function ActionStateComponent() {
     const {setTransactionState,transactionState} = useBlockchainStateContext()
 
-    setTransactionState("waitingWalletConnection")
+    
     const title = () :string => {
 
         let titleString : string = ""
         if (transactionState) {
-            if (     transactionState == "waitingWalletConnection" ) {
+            if (     transactionState == "waitingUserApproval" ) {
                 titleString = "Connect your wallet to Sepolia network"
             }
             if (     transactionState == "waitingUserApproval" ) {
@@ -20,10 +25,10 @@ export default function ActionStateComponent() {
                 titleString = "Confirming..."
             }
             if (     transactionState == "txRejected" ) {
-                titleString = "User rejected the reansaction"
+                titleString = "Transaction has been rejected"
             }
             if (     transactionState == "failedConfirmation" ) {
-                titleString = "Transaction failed. Please tyy again or contact support."
+                titleString = "Transaction failed. Please try again or contact support."
             }
             if (     transactionState ==  "confirmed") {
                 titleString = "Transaction submitted"
@@ -36,10 +41,43 @@ export default function ActionStateComponent() {
         return titleString
     }
   return (
-    <div className=' absolute z999 md:w-[25.5rem] md:h-[12.5rem] -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2 txStateBg flex flex-col'>
-        <h3 className='px-[10%]  mt-4 text-center !text-white font-semibold'>{title()}</h3>
-        <div className='mt-auto px-3 py-3'> <button onClick={() => setTransactionState(null) } className=' !py-2 !w-full outlineGreenButton'>Cancel</button></div>
-       
+    <>
+    {transactionState && 
+      <div className=' absolute z999 md:w-[25.5rem] md:h-[15rem] -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2 txStateBg flex flex-col'>
+      {/* <h3 className='px-[10%]  mt-4 text-center !text-white font-semibold'>{title()}</h3> */}
+      <h3 className='px-[10%]  mt-4 text-center !text-white font-semibold'>{title()}</h3>
+      {transactionState == "txRejected" && (
+        <div className='flex flex-grow'>    <div className='flex ml-auto mr-auto w-12 h-auto  mt-auto mb-auto '><RejectIcon/></div></div>
+
+)}
+      {transactionState == "failedConfirmation" && (
+        <div className='flex flex-grow'>    <div className='flex ml-auto mr-auto w-12 h-auto  mt-auto mb-auto '><FailedIcon/></div></div>
+
+)}
+      {transactionState == "confirmed" && (
+        <div className='flex flex-grow'>    <div className='flex ml-auto mr-auto w-14 h-auto  mt-auto mb-auto '><DoneIcon/></div></div>
+
+)}
+{transactionState == "waitingBlockchainConfirmation" && (
+    <div className='flex flex-grow relative'> 
+    {/* <Spinner color="success" size="lg" className='customSpinner ml-auto mr-auto absolute w-full mt-auto h-full '/> */}
+    <div className='flex ml-auto mr-auto w-full h-full  mt-auto mb-auto  absolute '>
+    <div className="spinner mt-auto mb-auto  ml-auto mr-auto "></div>
+   
     </div>
+    <div className='flex ml-auto mr-auto w-11 h-auto  mt-auto mb-auto  relative '>
+    <BlockdomLogo/>
+    </div>
+    </div>
+)}
+      { transactionState == "txRejected" || transactionState == "confirmed" || transactionState == "connectionRejected" || transactionState == "failedConfirmation" ? (
+            <div className='mt-auto px-3 py-3 flex flex-shrink'> <button onClick={() => setTransactionState(null) } className=' !py-2 !w-full outlineGreenButton'>Close</button></div>
+
+      ):""}
+
+
+  </div>}
+    </>
+  
   )
 }
---
+++ play-2-earn/blockdom/svg/failedIcon.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function FailedIcon() {
+  return (
+    <svg width="86" height="86" viewBox="0 0 86 86" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M34.0594 75.25H16.125C13.1565 75.25 10.75 72.8436 10.75 69.875V16.125C10.75 13.1565 13.1565 10.75 16.125 10.75H69.875C72.8436 10.75 75.25 13.1565 75.25 16.125V34.0961" stroke="#87F0E5" strokeOpacity="0.3" strokeWidth="7.16667" strokeLinecap="round" strokeLinejoin="round"/>
+<path d="M75.25 52.0205V73.4585C75.25 74.4481 74.4479 75.2502 73.4584 75.2502H52.0247" stroke="#87F0E5" strokeWidth="7.16667" strokeLinecap="round" strokeLinejoin="round"/>
+<path d="M75.25 52.0205H32.25" stroke="#87F0E5" strokeWidth="7.16667" strokeLinecap="round" strokeLinejoin="round"/>
+<path d="M41.2084 41.2084L30.4584 51.9584L41.2084 62.7084" stroke="#87F0E5" strokeWidth="7.16667" strokeLinecap="round" strokeLinejoin="round"/>
+</svg>
+
+  )
+}
