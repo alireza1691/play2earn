5e47f2d8b5dee57bef906a13c87726bd94b5affd
Alireza <alirezashadow52555@gmail.com>
2023-12-31 16:54:59 +0330

    map update

--- blockdom-app/components/gameComponents/battleLog/log.tsx
+++ blockdom-app/components/gameComponents/battleLog/log.tsx
@@ -3,15 +3,18 @@ import CoinIcon from "@/svg/coinIcon";
 
 import FoodIcon from "@/svg/foodIcon";
 import OpenIcon from "@/svg/openIcon";
+import WinIcon from "@/svg/winIcon";
 import Image from "next/image";
 import React from "react";
 
 export default function Log() {
+  const StatusConditions = ["Ongoing", "Attack", "Defense"] as const;
   const relevantButton = "Revenge";
+  let logStatus = "Ongoing";
   return (
     <div className="logBg  flex flex-row ">
-      <div className=" flex flex-col">
-        <div className="flex flex-row px-3 flex-shrink ">
+      <div className=" flex flex-col w-full">
+        <div className="flex flex-row px-3 w-full ">
           <h3 className=" items-center gap-2 text-[#98FBD7] flex flex-row text-[16px] font-light">
             <span className=" font-bold">Defender: </span> 0x...1234
             <OpenIcon /> <span className="ml-3 font-semibold">Land:</span>{" "}
@@ -22,10 +25,11 @@ export default function Log() {
             </span>
           </h3>
         </div>
-        <div className=" flex flex-row  h-full flex-grow bg-white/10 ">
-          <div className=" flex flex-row ">
+        <div className=" flex flex-row  h-full flex-grow  items-center gap-6 ">
+          <div className=" flex flex-row gap-1">
             {warriors.map((warrior, key) => (
               <Image
+                key={key}
                 src={"/svgs/images/warriorCard.svg"}
                 className="!w-[40px] h-auto"
                 width={40}
@@ -34,25 +38,91 @@ export default function Log() {
               />
             ))}
           </div>
-        
+          <WinIcon />
+          <div className=" flex flex-row gap-1">
+            {warriors.map((warrior, key) => (
+              <Image
+                key={key}
+                src={"/svgs/images/warriorCard.svg"}
+                className="!w-[40px] h-auto"
+                width={40}
+                height={20}
+                alt="warrior"
+              />
+            ))}
+          </div>
         </div>
       </div>
-      <div className=" ml-auto  h-full flex flex-col md:flex-row gap-2 py-2 px-2">
-        <div className=" flex flex-col gap-2 w-[182px]">
-          <div className="balBg  px-4 py-2 flex flex-row justify-start gap-4 items-center">
-            {" "}
-            <CoinIcon /> <p>12312</p>
-          </div>
-          <div className="balBg  px-4 py-2 flex flex-row justify-start gap-4 items-center">
-            <FoodIcon />
-            <p>12312234</p>{" "}
-          </div>
-        </div>
-        <div className="flex flex-col gap-2 w-[182px]">
-          <a className="logResultBg px-3 text-center py-3">You won</a>
-          <a className="logResultBg px-3 text-center  py-3">You won</a>
-        </div>
-      </div>
+      {logStatus == "Attack" && <AttackLogAction />}
+      {logStatus == "Ongoing" && <OngoingLogAction />}
+      {logStatus == "Defense" && <DefenseLogAction />}
     </div>
   );
 }
+
+export const AttackLogAction = () => {
+  return (
+    <div className=" ml-auto  h-full flex flex-col md:flex-row gap-2 py-2 px-2">
+      <div className=" flex flex-col gap-2 w-[182px]">
+        <div className="balBg  px-4 py-2 flex flex-row justify-start gap-4 items-center">
+          {" "}
+          <CoinIcon /> <p>12312</p>
+        </div>
+        <div className="balBg  px-4 py-2 flex flex-row justify-start gap-4 items-center">
+          <FoodIcon />
+          <p>12312234</p>{" "}
+        </div>
+      </div>
+      <div className="flex flex-col gap-2 w-[182px]">
+        <a className="logResultBg px-3 text-center py-3">You won</a>
+        <a className="logResultBg px-3 text-center  py-3">You won</a>
+      </div>
+    </div>
+  );
+};
+export const DefenseLogAction = () => {
+  return (
+    <div className=" ml-auto  h-full flex flex-col md:flex-row gap-2 py-2 px-2">
+      <div className=" flex flex-col gap-2 w-[182px]">
+        <div className="balBg  px-4 py-2 flex flex-row justify-start gap-4 items-center">
+          {" "}
+          <CoinIcon /> <p>12312</p>
+        </div>
+        <div className="balBg  px-4 py-2 flex flex-row justify-start gap-4 items-center">
+          <FoodIcon />
+          <p>12312234</p>{" "}
+        </div>
+      </div>
+      <div className="flex flex-col gap-2 w-[182px]">
+        <a className="logResultBg px-3 text-center py-3">You won</a>
+        <a className="logResultBg px-3 text-center  py-3">You won</a>
+      </div>
+    </div>
+  );
+};
+
+export const OngoingLogAction = () => {
+  return (
+    <div className=" ml-auto  h-full flex flex-col md:flex-row gap-2 py-2 px-2">
+      <div className=" flex flex-row gap-2 ">
+        <div className="w-[200px] h-full flex items-center justify-center  bg-[#06291D80]/50">
+          <a className="  ml-auto mr-auto  px-3 text-center py-3">
+            Army arrived at the enemy land
+          </a>
+        </div>
+        <div className=" flex flex-col justify-around">
+          {" "}
+          <a className=" rounded-full bg-white/10 w-6 h-6 border border-white/20"></a>
+          <a className=" rounded-full bg-white/10 w-6 h-6 border border-white/20"></a>
+          <a className=" rounded-full bg-white/10 w-6 h-6 border border-white/20"></a>
+        </div>
+      </div>
+      <div className="flex flex-col gap-2 w-[182px]">
+        <a className=" bg-[#06291D80]/50 px-3 text-center py-3">Ready</a>
+        <a className=" !rounded-md cursor-pointer greenButton !w-full px-3 text-center  py-3">
+          Start war
+        </a>
+      </div>
+    </div>
+  );
+};
--- blockdom-app/components/gameComponents/mapComponents/map.tsx
+++ blockdom-app/components/gameComponents/mapComponents/map.tsx
@@ -39,17 +39,17 @@ export default function Map() {
   }
 
   const landStyleMapView = (land:number)=> {
-    return ` xl:w-5 xl:h-5 w-3 h-3 bg-black/10 text-white text-[4px] ${getOwnerFromEvents(land) !== zeroAddress && " !bg-black/30"}`;
+    return ` xl:w-[10px] xl:h-[10px] w-[7px] h-[7px] bg-black/10 text-white  ${getOwnerFromEvents(land) !== zeroAddress && " !bg-black/30"}`;
 }
   const parcelStyleMapView =
-    "relative hover:bg-black/20 hover:brightness-75 transition-all cursor-pointer grid grid-cols-10 gap-[2px]  xl:!w-[13rem] !w-[8rem]  !h-auto ";
+    "relative gap-[1px] hover:bg-black/20  hover:brightness-75 transition-all cursor-pointer grid grid-cols-10   !w-max  !h-max ";
 
   const gridImages = [
     "/map/Outer3.png",
     "/map/Outer4.png",
     "/map/Outer5.png",
     "/map/Outer2.png",
-    "/map/centerParcel.png",
+    "/map/Base.png",
     "/map/Outer6.png",
     "/map/Outer1.png",
     "/map/Outer8.png",
@@ -73,28 +73,28 @@ export default function Map() {
     <>
       {selectedParcel == null && (
         <>
-          {/* <div className=" grid grid-cols-3 z-10  xl:h-[160rem] xl:w-[157rem]  bg-[url(/map/Base.png)] bg-cover ">
+          <div className=" grid grid-cols-3 z-10 w-[1920px] h-[110rem]   xl:h-[160rem] xl:w-[2560px]   ">
             {gridImages.map((col, key) => (
               <Image
-                className=" xl:h-[53rem] w-auto"
+                className=" !w-[160rem]"
                 src={col}
-                width={100}
-                height={100}
+                width={1024}
+                height={1024}
                 alt="map"
+                // quality={100}
               />
-            ))} */}
+            ))}
+            </div>
 
-            <div className='flex  z-10 w-[98rem] h-[110rem]   xl:h-[160rem] xl:w-[145rem]  bg-[url(/map/Base.png)] bg-cover overflow-hidden blur-sm '>
-        <div className=' absolute right-0 top-0 flex flex-col ml-auto -mt-[11.5rem] xl:-mt-[22.5rem]  w-auto'>
-        <Image className=' translate-x-2/3 blur-sm h-[27.25rem] xl:h-[41.5rem] w-auto' quality={10} src={"/map/Outer5.png"} width={100} height={100} alt='map'/>
-        <Image className=' translate-x-2/3 blur-sm h-[27.25rem] xl:h-[41.5rem] w-auto' quality={10} src={"/map/Outer6.png"} width={100} height={100} alt='map'/>
-        <Image className=' translate-x-2/3 blur-sm h-[27.25rem] xl:h-[41.5rem] w-auto' quality={10} src={"/map/Outer6.png"} width={100} height={100} alt='map'/>
-        <Image className=' translate-x-2/3 blur-sm h-[27.25rem] xl:h-[41.5rem] w-auto' quality={10} src={"/map/Outer6.png"} width={100} height={100} alt='map'/>
-        <Image className=' translate-x-2/3 blur-sm h-[27.25rem] xl:h-[41.5rem] w-auto' quality={10} src={"/map/Outer7.png"} width={100} height={100} alt='map'/>
+{/* <div className='flex  z-10 w-[1920px] h-[110rem]   xl:h-[160rem] xl:w-[2560px]  bg-[url(/map/Base.png)] bg-cover overflow-hidden  '>
+        <div className=' absolute right-[-58rem] top-0 flex flex-col  -mt-[21.5rem] xl:-mt-[33.5rem]  w-auto'>
+        <Image className='    h-[39rem] xl:h-[59.5rem] w-auto' quality={90} src={"/map/Outer5.png"}  width={1000} height={1000} alt='map'/>
+        <Image className='  h-[39rem] xl:h-[59.5rem] w-auto' quality={90} src={"/map/Outer6.png"} width={1000} height={1000} alt='map'/>
+        <Image className='  h-[39rem] xl:h-[59.5rem] w-auto' quality={90} src={"/map/Outer6.png"} width={1000} height={1000} alt='map'/>
+        <Image className=' h-[39rem] xl:h-[59.5rem] w-auto' quality={90} src={"/map/Outer7.png"} width={1000} height={1000} alt='map'/>
         </div>
         <div className=' absolute left-0 top-0 flex flex-col ml-auto -mt-[11.5rem] xl:-mt-[22.5rem]  w-auto'>
         <Image className=' -translate-x-2/3 blur-sm h-[27.25rem] xl:h-[41.5rem] w-auto' quality={10} src={"/map/Outer3.png"} width={100} height={100} alt='map'/>
-        <Image className=' -translate-x-2/3 blur-sm h-[27.25rem] xl:h-[41.5rem] w-auto' quality={10} src={"/map/Outer2.png"} width={100} height={100} alt='map'/>
         <Image className=' -translate-x-2/3 blur-sm h-[27.25rem] xl:h-[41.5rem] w-auto' quality={10} src={"/map/Outer2.png"} width={100} height={100} alt='map'/>
         <Image className=' -translate-x-2/3 blur-sm h-[27.25rem] xl:h-[41.5rem] w-auto' quality={10} src={"/map/Outer2.png"} width={100} height={100} alt='map'/>
         <Image className=' -translate-x-2/3 blur-sm h-[27.25rem] xl:h-[41.5rem] w-auto' quality={10} src={"/map/Outer1.png"} width={100} height={100} alt='map'/>
@@ -109,8 +109,32 @@ export default function Map() {
         <Image className='  blur-sm h-[26.75rem] xl:h-[39.5rem] w-auto' quality={10} src={"/map/Outer8.png"} width={100} height={100} alt='map'/>
         <Image className='  blur-sm h-[26.75rem] xl:h-[39.5rem] w-auto' quality={10} src={"/map/Outer8.png"} width={100} height={100} alt='map'/>
         </div>
-          </div>
-          <div className=" mt-[8.5rem] left-[7.5rem] xl:left-[6.25rem] z-10 absolute  grid grid-cols-10 gap-1 top-[4rem] !h-[88rem] w-[83rem] xl:h-[160rem] xl:w-[132rem] ">
+          </div> */}
+            {/* <div className='flex  z-10 w-[1920px] h-[110rem]   xl:h-[160rem] xl:w-[2560px]  bg-[url(/map/Base.png)] bg-cover overflow-hidden blur-sm '>
+        <div className=' absolute right-0 top-0 flex flex-col ml-auto -mt-[11.5rem] xl:-mt-[33.5rem]  w-auto'>
+        <Image className=' translate-x-[60%]  h-[27.25rem] xl:h-[57.5rem] w-auto' quality={90} src={"/map/Outer5.png"}  width={100} height={100} alt='map'/>
+        <Image className=' translate-x-2/3  h-[27.25rem] xl:h-[57.5rem] w-auto' quality={90} src={"/map/Outer6.png"} width={100} height={100} alt='map'/>
+        <Image className=' translate-x-2/3  h-[27.25rem] xl:h-[57.5rem] w-auto' quality={90} src={"/map/Outer6.png"} width={100} height={100} alt='map'/>
+        <Image className=' translate-x-2/3 blur-sm h-[27.25rem] xl:h-[41.5rem] w-auto' quality={10} src={"/map/Outer7.png"} width={100} height={100} alt='map'/>
+        </div>
+        <div className=' absolute left-0 top-0 flex flex-col ml-auto -mt-[11.5rem] xl:-mt-[22.5rem]  w-auto'>
+        <Image className=' -translate-x-2/3 blur-sm h-[27.25rem] xl:h-[41.5rem] w-auto' quality={10} src={"/map/Outer3.png"} width={100} height={100} alt='map'/>
+        <Image className=' -translate-x-2/3 blur-sm h-[27.25rem] xl:h-[41.5rem] w-auto' quality={10} src={"/map/Outer2.png"} width={100} height={100} alt='map'/>
+        <Image className=' -translate-x-2/3 blur-sm h-[27.25rem] xl:h-[41.5rem] w-auto' quality={10} src={"/map/Outer2.png"} width={100} height={100} alt='map'/>
+        <Image className=' -translate-x-2/3 blur-sm h-[27.25rem] xl:h-[41.5rem] w-auto' quality={10} src={"/map/Outer1.png"} width={100} height={100} alt='map'/>
+        </div>
+        <div className=' absolute left-1/2 -translate-x-[82%] xl:-translate-x-[81%] top-0 flex flex-row ml-auto -mt-[11.25rem] xl:-mt-[21rem]  w-auto'>
+        <Image className=' blur-sm h-[26.75rem] xl:h-[39.5rem] w-auto' quality={10} src={"/map/Outer4.png"} width={100} height={100} alt='map'/>
+        <Image className='  blur-sm h-[26.75rem] xl:h-[39.5rem] w-auto' quality={10} src={"/map/Outer4.png"} width={100} height={100} alt='map'/>
+        <Image className='  blur-sm h-[26.75rem] xl:h-[39.5rem] w-auto' quality={10} src={"/map/Outer4.png"} width={100} height={100} alt='map'/>
+        </div>
+        <div className=' absolute left-1/2 -translate-x-[82%] xl:-translate-x-[81%] bottom-0 flex flex-row ml-auto -mb-[14.25rem] xl:-mb-[23rem]  w-auto'>
+        <Image className=' blur-sm h-[26.75rem] xl:h-[39.5rem] w-auto' quality={10} src={"/map/Outer8.png"} width={100} height={100} alt='map'/>
+        <Image className='  blur-sm h-[26.75rem] xl:h-[39.5rem] w-auto' quality={10} src={"/map/Outer8.png"} width={100} height={100} alt='map'/>
+        <Image className='  blur-sm h-[26.75rem] xl:h-[39.5rem] w-auto' quality={10} src={"/map/Outer8.png"} width={100} height={100} alt='map'/>
+        </div>
+          </div> */}
+          <div className=" gap-1 top-[34rem] left-[34rem] xl:left-[44.5rem] xl:top-[45rem] z-10 absolute  grid grid-cols-10    !h-max !w-max  ">
             {allParcels.map((parcel, key) => (
               <div key={key} className={parcelStyleMapView}>
                 <a
@@ -118,7 +142,7 @@ export default function Map() {
                     setSelectedParcel(coordinatesObject(parcel[0])),
                       console.log(parcel[0]);
                   }}
-                  className=" active:backdrop-brightness-50 opacity-50 xl:opacity-10 xl:hover:opacity-100 h-full w-full flex items-center justify-center   text-center absolute z-50 text-white  text-[25px]"
+                  className=" active:backdrop-brightness-50 opacity-50 xl:opacity-10 xl:hover:opacity-100 h-full w-full flex items-center justify-center   text-center absolute z-50 text-white  text-[16px]"
                 >
                   {parcel[0] - 9}
                 </a>
