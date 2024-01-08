aafd83ef7eb2b2fa7636799f04542befd32d8acb
Alireza <alirezashadow52555@gmail.com>
2024-01-08 13:44:54 +0330

    fix build issue

--- blockdom-app/components/gameComponents/buildingPopUpWindow/upgradeContainer.tsx
+++ blockdom-app/components/gameComponents/buildingPopUpWindow/upgradeContainer.tsx
@@ -1,15 +1,17 @@
 import { useSelectedBuildingContext } from '@/context/selected-building-context'
+import { useUserDataContext } from '@/context/user-data-context'
 import DoubleArrow from '@/svg/doubleArrow'
 import React from 'react'
 
 export default function UpgradeContainer() {
 
     const {selectedItem} = useSelectedBuildingContext()
+    const {inViewLand} = useUserDataContext()
   return (
     <div className="z-10 flex flex-row mt-3 gap-8">
-    <h3 className=" font-semibold">Level {selectedItem?.level}</h3>
+    <h3 className=" font-semibold">Level {Number(inViewLand?.townhallLvl)}</h3>
     <DoubleArrow />
-    <h3 className=" font-semibold">Level {Number(selectedItem?.level) + 1}</h3>
+    <h3 className=" font-semibold">Level {Number(inViewLand?.townhallLvl) + 1}</h3>
   </div>
   
   )
